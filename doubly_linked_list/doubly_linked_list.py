35241ba57b49c98f477a3a6c2b735718b254224d
Pedro Escobedo <pedro.escobedo.design@gmail.com>
2020-09-20 22:18:27 -0700

    doubly linked list update

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -62,7 +62,16 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.tail and not self.head:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            new_node.next = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
             
     """
     Removes the List's current tail node, making the 
@@ -70,21 +79,40 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
+
     def move_to_front(self, node):
-        pass
-        
+        if node is self.head:
+            return None
+        value = node.value
+        if node is self.tail:
+            self.remove_from_head()
+        else:
+            node.delete()
+            self.length -= 1
+        self.add_to_head(value)
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return None
+        value = node.value
+        if node is self.head:
+            self.remove_from_head()
+        else:
+            node.delete()
+            self.length -= 1
+        self.add_to_tail(value)
 
     """
     Deletes the input node from the List, preserving the 
--- queue/queue.py
+++ queue/queue.py
@@ -14,27 +14,121 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
-import sys
-sys.path.append("../singly_linked_list")
-from singly_linked_list import *
+class Node:
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, new_next):
+    self.next_node = new_next
+
+class LinkedList(Node):
+  def __init__(self, first_node=None):
+    self.head = first_node
+    self.tail = first_node
+
+  def add_to_head(self, value, tail):
+    # create a new Node
+    new_node = Node(value)
+    if self.head is None:
+      # create a new Node
+      self.head = new_node
+      self.tail = new_node
+    else:
+      # set next_node of my new Node to the head
+      new_node.set_next_node(self.head)
+      # update head attribute
+      self.head = new_node
+
+  def add_to_tail(self, value):
+    # create a new Node
+    new_node = Node(value)
+    # 1. LL is empty
+      # update to head & tail attributes
+    if self.tail is None:
+      # update head & tail attributes
+      self.tail = new_node
+      self.head = new_node
+    else:
+      # set next_node of my new Node to the head
+      new_node.set_next_node(new_node)
+      # update head attribute
+      self.tail = new_node
+
+  def remove_head(self):
+    if self.head is None:
+        return None
+    else:
+      head_value = self.head.get_value()
+      if self.head == self.tail:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
+      return head_value
+
+  def remove_tail(self):
+    if self.tail is None and self.head is None:
+      return None
+    else:
+      tail_value = self.tail.get_value()
+      if self.tail == self.head:
+        self.head = None
+        self.tail = None
+      else:
+        head_value = self.head
+        if head_value.get_next_node() != self.tail:
+          head_value = head_value.get_next_node()
+          head_value.set_next_node(None)
+          self.tail = head_value
+      return tail_value
+
+  def contains(self, value):
+    head_value = self.head
+    tail_value = self.tail
+    if head_value is not None and tail_value is not None:
+      return head_value.get_value()
+    else:
+      return None
+
+  def get_max(self):
+    head_value = self.head
+    max_value = []
+    if head_value is None:
+      return None
+    else:
+      while head_value != self.tail:
+        max_value.append(head_value.get_value())
+        head_value = head_value.get_next_node()
+      max_value.append(self.tail.get_value())
+      return max(max_value)
+
 
 class Queue(LinkedList):
     def __init__(self):
-        super().__init__()
+        # super().__init__()
         self.size = 0
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def enqueue(self, item):
-        self.add_to_tail(item)
+        # self.add_to_tail(item)
         self.size += 1
+        self.storage.add_to_tail(item)
 
     def dequeue(self):
         if self.size == 0:
             return None
         self.size -= 1
-        return self.remove_head()
+        return self.storage.remove_head()
 
 if __name__ == "__main__":
     a = Queue()
